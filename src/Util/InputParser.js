78e87110fcde93fec07a13d22b1a820a9c032137
flaviowildner <flaviowildner@hotmail.com>
2019-02-25 05:51:40 -0300

    Dealing with huge amount of data and some comments

--- src/Util/InputParser.js
+++ src/Util/InputParser.js
@@ -1,6 +1,8 @@
+const MAX_POINTS_PER_SERIES = 10;
+
 function strToJson(str) {
 	var inputSplitted, inputParsed;
-	inputSplitted = str.split('\n'); // Convert json input string to valid json
+	inputSplitted = str.split('\n'); // Split the entire input in lines of string json
 	inputSplitted = inputSplitted.filter(el => el.trim()); // Remove empty and only whitespaces lines
 	inputParsed = inputSplitted.map((el) => JSON.parse(JSON.stringify(eval("(" + el + ")")))); // Parse JSON
 	return inputParsed;
@@ -10,7 +12,7 @@ function generateChartState(inputEventsString) {
 	let select, group, begin, end, data = [];
 	let inputParsed, indexLastStart;
 
-	try{
+	try {
 		inputParsed = strToJson(inputEventsString);
 		inputParsed.sort((a, b) => { // Sort events based on timestamp. If the timestamp is equal, the priority is used to tie break.
 			const priority = { "start": 1, "span": 2, "data": 3, "stop": 4 };
@@ -20,15 +22,16 @@ function generateChartState(inputEventsString) {
 			if (priority[a.type] > priority[b.type]) return 1;
 			return 0;
 		});
-		indexLastStart = inputParsed.length - inputParsed.slice().reverse().findIndex((el) => { //Find the last start type event
+		indexLastStart = inputParsed.length - inputParsed.slice().reverse().findIndex((el) => { //Find the last start type event. It is unnecesary to process all events.
 			return el.type === 'start'
 		}) - 1;
 
-		if(indexLastStart === inputParsed.length) throw Error('There\'s no event of start type')
+
+		if (indexLastStart === inputParsed.length) throw Error('There\'s no event of start type')
 		for (let i = indexLastStart; i < inputParsed.length; i++) {
 			const currEvent = inputParsed[i];
 			const { type, timestamp } = currEvent;
-			if(timestamp < 0) throw Error('The timestamp of event should be greater than zero')
+			if (timestamp < 0) throw Error('The timestamp of event should be greater than zero')
 			switch (type) {
 				case 'start':
 					if (!currEvent.select) throw Error('Event of start type should have a set of select')
@@ -45,14 +48,16 @@ function generateChartState(inputEventsString) {
 					break;
 				case 'data':
 					if (timestamp < begin || timestamp > end) break;
-					select.forEach((currSelect) => {
-						if (currEvent[currSelect]) {
-							const groupName = group.reduce((r, c) => currEvent[r] + '_' + currEvent[c]);
-							const lineName = groupName + '-' + currSelect;
-							if (!data[lineName]) data[lineName] = { data: [] }
-							data[lineName] = { name: lineName, data: { ...data[lineName].data, [currEvent.timestamp]: currEvent[currSelect] } };
-						}
-					});
+					if (select) {
+						select.forEach((currSelect) => {
+							if (currEvent[currSelect]) {
+								const groupName = group.reduce((r, c) => currEvent[r] + '_' + currEvent[c]);
+								const lineName = groupName + '-' + currSelect;
+								if (!data[lineName]) data[lineName] = { data: [] }
+								data[lineName] = { name: lineName, data: { ...data[lineName].data, [currEvent.timestamp]: currEvent[currSelect] } };
+							}
+						});
+					}
 					break;
 				case 'stop':
 					i = inputParsed.length;
@@ -61,17 +66,36 @@ function generateChartState(inputEventsString) {
 					throw Error('Invalid event type')
 			}
 		}
+		//Dealing with huge amount of data
+		/*
+		My solution was to take a sample of the data by defining the maximum number of points that a series can have. I set that number to 10.
+		In this way, it is possible to see more easily the long-term variation of the series. But in cases where it is necessary to see more detail
+		of the data, this is not a good solution due the loss of data.
+		There is some solutions that I thought of how to deal with different problems of huge amount of data.
+		If it is necessary to know the trend of values, linear regression can be used.
+		If the detail of the data are very important, paging can be used by spliting in sub-charts. Or even panning(sliding horizontally) if the amount of data is not so large 
+		to slow performance.
+		*/
 		const newChartState = Object.keys(data).map((p) => data[p]);
+		newChartState.forEach((el, idx) => {
+			const dataLenght = Object.keys(el.data).length;
+			Object.keys(el.data).forEach((el_id, idx) => {
+				if ((idx % parseInt(dataLenght / MAX_POINTS_PER_SERIES))) {
+					delete el.data[el_id];
+				}
+			});
+		});
+
 		return {
 			data: newChartState,
 		}
-	} catch(err){
+	} catch (err) {
 		return {
 			data: [],
 			errorMessage: err.message
 		}
 	}
-	
+
 }
 
 export { generateChartState }
